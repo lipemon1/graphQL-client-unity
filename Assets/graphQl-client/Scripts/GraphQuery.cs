0569f4bc4a01d6e8ddc19263f6c7202807a15748
Ramon Ferreira <ramon_felipe@outlook.com>
2019-09-20 13:47:32 -0300

    Refactoring to Unity Web Request
    Insteading of using WWW requests now the code is using the UnityWebRequest system.

--- Assets/graphQl-client/Scripts/GraphQuery.cs
+++ Assets/graphQl-client/Scripts/GraphQuery.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace graphQLClient
 {
@@ -46,44 +47,47 @@ namespace graphQLClient
 		public static Dictionary<string, string> variable = new Dictionary<string, string>();
 		public static Dictionary<string, string[]> array = new Dictionary<string, string[]>();
 
-		public static WWW POST(string details)
+		public static void POST(string details)
 		{
 			details = QuerySorter(details);
-			Query query = new Query();
-			string jsonData = "";
-			WWWForm form = new WWWForm();
-			query = new Query { query = details };
-			jsonData = JsonUtility.ToJson(query);
-			byte[] postData = Encoding.ASCII.GetBytes(jsonData);
-			Dictionary<string, string> postHeader = form.headers;
-			if (postHeader.ContainsKey("Content-Type")){
-				postHeader.Add("Authorization", authToken);
-				postHeader["Content-Type"] = "application/json";
-			}
-				
-			else
-				postHeader.Add("Content-Type", "application/json");
-
-			WWW www = new WWW(url, postData, postHeader);
-			instance.StartCoroutine(WaitForRequest(www));
+			var query = new Query { query = details };
+			var jsonData = JsonUtility.ToJson(query);
+			instance.StartCoroutine(GraphQlPost(url, jsonData));
 			queryStatus = Status.Loading;
-			return www;
 		}
 
-		static IEnumerator WaitForRequest(WWW data)
+		static IEnumerator GraphQlPost(string url, string jsonData)
 		{
-			yield return data; // Wait until the download is done
-			if (data.error != null)
+			var uwr = new UnityWebRequest(url, "POST");
+			uwr.uploadHandler = new UploadHandlerRaw(new System.Text.UTF8Encoding().GetBytes(jsonData));
+			uwr.downloadHandler = new DownloadHandlerBuffer();
+
+			if (uwr.GetRequestHeader("Content-Type") != null)
 			{
-				Debug.Log("There was an error sending request: " + data.error);
-				queryStatus = Status.Error;
+				uwr.SetRequestHeader("Authorization", authToken);
+				uwr.SetRequestHeader("Content-Type", "application/json");	
 			}
 			else
+				uwr.SetRequestHeader("Content-Type", "application/json");
+
+			using (uwr)
 			{
-				queryReturn = data.text;
-				queryStatus = Status.Complete;
+				yield return uwr.SendWebRequest();
+
+				if (uwr.isNetworkError || uwr.isHttpError)
+				{
+					Debug.Log("UWR: There was an error sending request: " + uwr.error);
+					queryStatus = Status.Error;
+				}
+				else
+				{
+					Debug.Log("UWR Response: " + uwr.downloadHandler.text);
+					queryReturn = uwr.downloadHandler.text;
+					queryStatus = Status.Complete;
+				}
+				
+				onQueryComplete();
 			}
-			onQueryComplete();
 		}
 
 		public static string QuerySorter(string query)
